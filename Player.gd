c43dd36229e35bf9adcd992ca8d32a85ed91733d
Ojas Sanghi <ojas.sanghi@protonmail.com>
2019-11-24 11:25:19 -0700

    add comments and function return types

--- Player.gd
+++ Player.gd
@@ -16,8 +16,8 @@ var times_jumped = 0
 # cache the sprite here for fast access (we will set scale to flip it often)
 onready var sprite = $Sprite
 
-func get_input():
-	# Detect up/down/left/right keystate and only move when pressed
+func get_input() -> int:
+	# return desired left-right movement
 	var target_speed = 0
 	if Input.is_action_pressed('ui_right'):
 		target_speed += 1
@@ -26,7 +26,7 @@ func get_input():
 
 	return target_speed
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 
 	# Apply gravity
 	linear_vel += delta * GRAVITY_VEC
@@ -44,6 +44,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		# only let them double jump if they have that powerup
 		if Globals.has_powerup("double_jump"):
+			# ensure they can only double jump
 			if times_jumped < 1:
 				linear_vel.y = -JUMP_SPEED
 				times_jumped += 1
@@ -59,6 +60,8 @@ func _physics_process(delta):
 	var new_anim = "idle"
 
 	if on_floor:
+		# If they hit the ground after the falling, play the hit_ground animation
+		# But it won't do this if they land in water; the swim animation will play then
 		if anim == "fall" and not Globals.in_water:
 			$AnimationPlayer.play("hit_ground")
 			yield($AnimationPlayer, "animation_finished")
@@ -79,11 +82,14 @@ func _physics_process(delta):
 		if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 			sprite.set_flip_h(false)
 
+		# If they're going up, set the jumping animation
+		# Otherwise, set the fall animation
 		if linear_vel.y < 0:
 			new_anim = "jump"
 		else:
 			new_anim = "fall"
 
+	# swim animation if they're in the water
 	if Globals.in_water:
 		new_anim = "swim"
 
