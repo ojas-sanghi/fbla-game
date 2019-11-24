4e8964d038d8e0beeb06880a0c38c5f898778d69
Sajo8 <andrewsplin@protonmail.com>
2019-11-24 10:48:12 -0700

    fix extents of water area collisionshape

--- waterArea.gd
+++ waterArea.gd
@@ -1,25 +1,19 @@
 extends Area2D
 
-#var in_water: bool = false
-
 func _ready() -> void:
 
 	var shape = RectangleShape2D.new()
 	shape.extents = get_scale()
-	print(get_scale())
-	print(shape.extents)
+	shape.extents.x = shape.extents.x / 4
 	$CollisionShape2D.shape = shape
 
 func _on_Area2D_body_entered(body: PhysicsBody2D) -> void:
 	if body:
-		if body.name != "Player":
-			return
-	else:
-		return
-	Globals.in_water = true
-#	while(Globals.in_water):
-#		pass
-
+		if body.name == "Player":
+			Globals.in_water = true
+	return
 
 func _on_Area2D_body_exited(body: PhysicsBody2D) -> void:
-	Globals.in_water = false
+	if body:
+		if body.name == "Player":
+			Globals.in_water = false
