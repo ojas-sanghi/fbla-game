adf03d864ab6f18fccce1c654b413b8096d2e57f
Andrew Splin <andrewsplin@protonmail.com>
2020-01-16 23:37:28 -0700

    basic movement and double jump
    no animations yet!

---
+++ actors/Actor.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(300.0, 500.0)
+export var gravity: = 3000.0
+
+var _velocity: = Vector2.ZERO
+
+
