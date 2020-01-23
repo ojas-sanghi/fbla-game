9b506370a037fd4c92dbeea8e7368a0ad56baa39
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-01-22 22:02:24 -0700

    Add scene transitions (fade to black)

--- sceneElements/portals/RedPortal.gd
+++ sceneElements/portals/RedPortal.gd
@@ -1,6 +1,18 @@
 extends Area2D
 
+onready var anim_player := $AnimationPlayer
+
+func _ready() -> void:
+	anim_player.play("start")
+	yield(anim_player, "animation_finished")
+	anim_player.play("portal")
+
+func teleport():
+	anim_player.play("fade_in")
+	yield(anim_player, "animation_finished")
+	get_tree().change_scene_to(load("res://levels/level1/Level1.tscn"))
+
 func _on_RedPortal_body_entered(body: PhysicsBody2D) -> void:
 	if body.name == "Player":
 		if Globals.has_powerup("double_jump"):
-			get_tree().change_scene_to(load("res://levels/level1/Level1.tscn"))
+			teleport()
