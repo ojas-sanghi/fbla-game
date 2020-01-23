d601502eb74486d4861e6e4e63ce90dc31d8f880
Sajo8 <33220209+Sajo8@users.noreply.github.com>
2020-01-23 16:25:14 -0700

    Change spawn position and remove powerups upon death

--- levels/level1/Level1.gd
+++ levels/level1/Level1.gd
@@ -3,6 +3,10 @@ extends Node2D
 func _ready():
 	# Set camera limit to the position of the last tile in the level
 	$Player/Camera2D.limit_right = 6272
+	
+	if Globals.player_died:
+		Globals.respawn_position = Vector2(374, 677)
+		Globals.player_died = false
 
 	var player = get_tree().get_nodes_in_group("player")
 	if player:
