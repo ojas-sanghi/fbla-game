214fe4af7d0503a89f17cde2c7f29fb9c68f3f5f
Andrew Splin <andrewsplin@protonmail.com>
2020-01-24 20:01:58 -0700

    Remove signals for direct method calls

--- sceneElements/flags/EndFlag.gd
+++ sceneElements/flags/EndFlag.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
-signal level_passed
-
 func _on_EndFlag_body_entered(body: PhysicsBody2D) -> void:
 	if body.name == "Player":
-		emit_signal("level_passed")
+		Globals.add_powerups_gained_this_level()
+		print("yay")
