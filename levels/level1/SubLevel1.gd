d2ac8850aed1f6e7aff0dacdf7c99933a648a6bc
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-01-24 19:28:19 -0700

    fix death bug

--- levels/level1/SubLevel1.gd
+++ levels/level1/SubLevel1.gd
@@ -3,5 +3,9 @@ extends Node2D
 func _ready():
 	# Set camera limit to the position of the bottom-most tile in the level
 	$Player/Camera2D.limit_bottom = 2175
-	
+
 	Globals.add_powerup("double_jump")
+
+	# Upon the re-loading of the level, the player has already spawned at the start point
+	# If this remains true, then if they die in the sublevel and go back to the main level, they'll spawn at the starting
+	Globals.player_died = false
