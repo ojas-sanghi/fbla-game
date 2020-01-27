b6c6b7448049bba6b395106734afd36ac2c4eaa3
Sajo8 <33220209+Sajo8@users.noreply.github.com>
2020-01-27 08:55:27 -0700

    Start adding comments to code

--- sceneElements/flags/EndFlag.gd
+++ sceneElements/flags/EndFlag.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 func _on_EndFlag_body_entered(body: PhysicsBody2D) -> void:
+	# Once the player reaches the green flag
 	if body.name == "Player":
+		# Permanently add the powerups they got this level
 		Globals.add_powerups_gained_this_level()
+		# For now just print yay, later on save progress and move to next level.
 		print("yay")
--- sceneElements/portals/BluePortal.gd
+++ sceneElements/portals/BluePortal.gd
@@ -5,21 +5,30 @@ onready var anim_player := $AnimationPlayer
 export var scene_path : String
 
 func _ready() -> void:
+	# Hide the black rectange (later used in fade anim) and make it transparent
 	anim_player.play("start")
 	yield(anim_player, "animation_finished")
+	
+	# Play the "idle" portal animation; the 2 frames
 	anim_player.play("portal")
 
 func teleport():
+	# Play the fade in animation and wait till it finishes
 	anim_player.play("fade_in")
 	yield(anim_player, "animation_finished")
+	
+	# Load the path set by us when making the scene
 	get_tree().change_scene_to(load(scene_path))
 
 func _on_BluePortal_body_entered(body: PhysicsBody2D) -> void:
 	if body.name == "Player":
+		# Set the respawn position to right of the entrance of the blue portal
+		# That way, when they finish the sublevel and come back to the main people, then they'll spawn here; it's a checkpoints of sorts
 		Globals.blue_respawn_position = Vector2(3900, 420)
 
 		var player = get_tree().get_nodes_in_group("player")
 		if player:
+			# Stop the movement and animation of the player
 			player[0].set_physics_process(false)
 			player[0].get_node("AnimationPlayer").stop(false)
 
