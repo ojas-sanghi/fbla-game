1535ff5266fc82d279ea3a0152997b274de8b938
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-01-27 20:10:19 -0700

    Add double jump, add some missing tiles

---
+++ props/DoubleJump.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+func _ready():
+	# Increase size
+	$Tween.interpolate_property($Sprite, 'scale',
+		$Sprite.get_scale(), Vector2(0.75, 0.75), 0.6,
+		Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	# Fade out
+	$Tween.interpolate_property($Sprite, 'modulate',
+		Color(1, 1, 1, 1), Color(0, 0, 0, 0), 0.6,
+		Tween.TRANS_LINEAR, Tween.EASE_OUT)
+
+func _on_DoubleJump_body_entered(body: PhysicsBody2D) -> void:
+	if body.name == "Player":
+		Globals.add_powerup("double_jump")
+
+		# Remove collision shape in order to prevent any more collisions whilst the effect is taking place
+		shape_owner_clear_shapes(get_shape_owners()[0])
+		# Stop animation
+		$AnimationPlayer.stop()
+		# Start tween effect
+		$Tween.start()
+		# Wait for effect to finish
+		yield($Tween, "tween_completed")
+		# Get rid of powerup
+		queue_free()
