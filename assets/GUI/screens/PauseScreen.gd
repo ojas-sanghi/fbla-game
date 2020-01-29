8f0f4456b76cd665ff0a4a7a0e354c0360449128
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-01-28 22:24:42 -0700

    Add pause and title screen, minor font changes, add button theme

---
+++ assets/GUI/screens/PauseScreen.gd
@@ -0,0 +1,23 @@
+extends Control
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		togglePause()
+
+func togglePause() -> void:
+	# Set the paused and visible status to the opposite of what they currently are
+	get_tree().paused = not get_tree().paused
+	visible = not visible
+
+	$VBoxContainer/Resume.grab_focus()
+
+func _on_Resume_pressed() -> void:
+	togglePause()
+
+func _on_QuitMenu_pressed() -> void:
+	$AnimationPlayer.play("fade_in")
+	yield($AnimationPlayer, "animation_finished")
+	get_tree().change_scene("res://assets/GUI/screens/TitleScreen.tscn")
+
+func _on_QuitDesktop_pressed() -> void:
+	get_tree().quit()
---
+++ assets/GUI/screens/TitleScreen.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready() -> void:
+	$VBoxContainer/Play.grab_focus()
+
+func _on_Play_pressed() -> void:
+	print("bruh")
+	$AnimationPlayer.play("fade_in")
+	yield($AnimationPlayer, "animation_finished")
+
+	get_tree().change_scene("res://levels/level1/Level1.tscn")
+
+func _on_QuitDesktop_pressed() -> void:
+	get_tree().quit()
