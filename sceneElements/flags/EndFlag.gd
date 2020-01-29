f79ab3e6ef0e935a45c1c742fd82842f5377403f
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-01-29 14:48:52 -0700

    Add themes to all labels, instead of instancing separate scenes

--- sceneElements/flags/EndFlag.gd
+++ sceneElements/flags/EndFlag.gd
@@ -5,6 +5,5 @@ func _on_EndFlag_body_entered(body: PhysicsBody2D) -> void:
 	if body.name == "Player":
 		# Permanently add the powerups they got this level
 		Globals.add_powerups_gained_this_level()
-		print(Globals.powerups)
 		# For now just print yay, later on save progress and move to next level.
 		print("yay")
